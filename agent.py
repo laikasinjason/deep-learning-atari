a01de9d049b7ae7d55a85cf37c11fd2576544d4c
laikasinjason <laikasin93@yahoo.com.hk>
2018-09-12 23:43:24 +0800

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,44 @@
+from IPython.core.debugger import set_trace
+class Agent:
+    # Class to save states
+
+    def __init__(self):
+        self.start_states = []
+        self.end_states = []
+        self.is_done = False
+        self.total_reward = 0
+        self.iteration = 0 # used for recording of iteration
+
+        
+    def reset_env(self, env, pre_process):
+        # First learning trial
+        frame = env.reset()
+        state = pre_process.preprocess(frame)
+        self.start_states = [state, state, state, state]
+        self.end_states = self.start_states
+        self.is_done = False
+        
+    def do_action(self, env, pre_process, action):
+        total_reward = 0
+        # end_state is start_state1
+        frame, reward, is_done, _ = env.step(action)
+        state = pre_process.preprocess(frame)
+        total_reward = total_reward + reward
+
+#         frame, reward, is_done, _ = env.step(action)
+#         state2 = pre_process.preprocess(frame)
+#         total_reward = total_reward + reward
+        
+#         frame, reward, is_done, _ = env.step(action)
+#         state3 = pre_process.preprocess(frame)
+#         total_reward = total_reward + reward
+        
+#         frame, reward, is_done, _ = env.step(action)
+#         state4 = pre_process.preprocess(frame)
+#         total_reward = total_reward + reward
+        
+        self.start_states = self.end_states.copy()
+        self.end_states.pop()
+        self.end_states.append(state)
+        self.is_done = is_done
+        self.total_reward = pre_process.transform_reward(total_reward)
---
+++ logger.py
@@ -0,0 +1,34 @@
+class Logger:
+    
+    # Class for saving the performance indicators
+
+    def __init__(self, evaluation_number):
+        self.sum_ret = 0.0 # reward in one play
+        self.total_ret = 0.0 # total reward in all plays
+        self.evaluation_number = evaluation_number
+        self.runs_in_eval = 0
+        self.num_evals = 1 # counter
+        self.max_return = 0
+        self.min_return = 9999999
+
+    def write(self, agent):
+        self.sum_ret += agent.total_reward
+        self.total_ret += agent.total_reward
+        if agent.is_done:
+            print ("Evaluation " + str(self.runs_in_eval) + " :" + str(self.sum_ret))
+            if self.sum_ret > self.max_return:
+                self.max_return = self.sum_ret
+            if self.sum_ret < self.min_return:
+                self.min_return = self.sum_ret
+            self.runs_in_eval += 1
+            self.sum_ret = 0.0
+
+            if self.runs_in_eval == self.evaluation_number:
+                self.runs_in_eval = 0
+                self.num_evals += 1
+                avg = float(self.total_ret)/float(self.evaluation_number)
+                print("Max reward : " + str(self.max_return) + "Min reward: " + str(self.min_return)\
+                      + "Average reward in current episode: " + str(avg))
+                self.total_ret = 0.0
+                f = open("model_learn_progress.txt", "a")
+                f.write(agent.iteration+","+str(avg)+","+str(self.max_return)+","+str(self.min_return)+"\n")
