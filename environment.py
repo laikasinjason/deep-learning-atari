e04c4a00c58d4ec3d9b5eecf0c02b6bd9b53cea8
Jason Lai <laikasin93@yahoo.com.hk>
2019-03-12 17:56:39 +0800

    Revert wrong commit

--- environment.py
+++ environment.py
@@ -1,141 +1,102 @@
+from keras import backend as K
+import keras
 import random
-
 import numpy as np
-import pandas as pd
-
-import data_engineering.data_engineering as data_engineering
-from buy_order_agent import BuyOrderAgent
-from buy_signal_agent import BuySignalAgent
-from sell_order_agent import SellOrderAgent
-from sell_signal_agent import SellSignalAgent
 
 
-class Environment:
-    class State:
-        def __init__(self, date, value):
-            self.date = date
-            self.value = value
+def fit_batch(model, target_model, start_states, actions, rewards, next_states, is_terminate, pre_process, 
+              gamma = 0.99):
+    """Do one deep Q learning iteration.
+    
+    Params:
+    - model: The DQN
+    - target_model: The target DQN
+    - gamma: Discount factor (should be 0.99)
+    - start_states: numpy array of starting states
+    - actions: numpy array of one-hot encoded actions coend_staterresponding to the start states
+    - rewards: numpy array of rewards corresponding to the start states and actions
+    - next_states: numpy array of the resulting states corresponding to the start states and actions
+    - is_terminate: numpy boolean array of whether the resulting state is terminal
+    
+    """
+    # convert to a 4-d array
+    next_states = np.stack(next_states).swapaxes(1,2).swapaxes(2,3)
+    start_states = np.stack(start_states).swapaxes(1,2).swapaxes(2,3)
+    next_states = pre_process.to_float(next_states)
+    start_states = pre_process.to_float(start_states)
+            
+    # Run one fast-forward to get the Q-values for all actions
+    target = model.predict(start_states)
+    current_Q_values = target[actions.astype(bool)]
+    
+    # Predict the Q values of the next states. Passing ones as the mask.
+#     next_Q_values = target_model.predict([next_states,  np.ones(actions.shape)])
+    next_Q_values = target_model.predict(next_states)
+    
+    # The Q values of the terminal states is 0 by definition, so override them
+    next_Q_values[is_terminate.astype(bool)] = 0
+    # The Q values of each start state is the reward + gamma * the max next state Q value
+    new_Q_values = rewards + gamma * np.max(next_Q_values, axis=1)
+    # Fit the keras model. Pass the actions as the mask and multiplying
+    # the targets by the actions.
+    
+    # Set the new Q values to target
+    target[actions.astype(bool)] = new_Q_values
+    
+    loss = model.fit(start_states, target,
+        epochs=1, batch_size=len(start_states), verbose=0
+    )
+    return loss
+    
+    
+def get_epsilon_for_iteration(current_iteration, stable_iteration = 1000000 , initial_epsilon = 1, end_epsilon=0.1):
+    '''
+    decrease the epsilon linearly from 1 to 0.1 over the first million frames, and fixed at 0.1 thereafter
+    '''
+    epsilon = end_epsilon
 
-    def __init__(self):
-        self.data = None
-        self.transaction_cost = 1
-        data_engineering.enrich_market_data(self.data)
+    if current_iteration <= stable_iteration:
+        decrease_per_epsilon = (initial_epsilon - end_epsilon) / stable_iteration
+        epsilon = initial_epsilon - current_iteration * decrease_per_epsilon
 
-        self.turning_point_max, self.turning_point_min = data_engineering.create_turning_point_3d_matrix(self.data)
-        self.tech_indicator_matrix = data_engineering.create_technical_indicator_3d_matrix(self.data)
+    return epsilon
 
-        # simulate data for testing
-        self.data = pd.DataFrame(
-            {'date': [i for i in range(100)], 'low': [2 * i for i in range(100)], 'high': [7 * i for i in range(100)],
-             'ma5': [3 * i for i in range(100)], 'close': [5 * i for i in range(100)]}).set_index('date')
-        self.turning_point_max = pd.DataFrame(
-            {'date': [i for i in range(100)], 'col2': [2 * i for i in range(100)]}).set_index('date')
-        self.turning_point_min = pd.DataFrame(
-            {'date': [i for i in range(100)], 'col2': [2 * i for i in range(100)]}).set_index('date')
-        self.tech_indicator_matrix = pd.DataFrame(
-            {'date': [i for i in range(100)], 'col2': [2 * i for i in range(100)]}).set_index('date')
+def choose_best_action(model, states, no_actions):
+    # swap axes to archieve keras model input
+    state = np.array(states).swapaxes(0,1).swapaxes(1,2)
+    # extend to a 4-d array
+    state = np.expand_dims(state, axis=0)
+#     return model.predict([state, np.expand_dims(np.ones(no_actions), axis=0)]).argmax()
+    return model.predict(state).argmax()
 
-    @staticmethod
-    def get_random_action(agent):
-        global action
-        if isinstance(agent, BuyOrderAgent):
-            action = random.randint(0, 7)
-        elif isinstance(agent, SellOrderAgent):
-            action = random.randint(0, 7)
-        elif isinstance(agent, BuySignalAgent):
-            action = random.randint(0, 1)
-        elif isinstance(agent, SellSignalAgent):
-            action = random.randint(0, 1)
-        return action
+def q_iteration(env, model, target_model, agent, iteration, ring_buf, one_hot_encoder, pre_process):
+    # Choose epsilon based on the iteration
+    epsilon = get_epsilon_for_iteration(iteration)
 
-    def get_sell_signal_states_by_date(self, bp, date):
-        # get next day state, if next day state is not available, throws error
-        try:
-            td_market_data = self.data.loc[date]['close']
-            profit = (td_market_data - bp) / bp
+    # Choose the action
+    if random.random() < epsilon:
+        action = env.action_space.sample()
+    else:
+        action = choose_best_action(model, agent.end_states, env.action_space.n)
 
-            temp_series = pd.Series([profit])
-            profit_bin = pd.get_dummies(pd.cut(temp_series, data_engineering.bins, labels=data_engineering.names))
+    # Play one game iteration (action X 4 times)
+    agent.do_action(env, pre_process, action)
+    ring_buf.append((agent.start_states, action, agent.total_reward, agent.end_states, agent.lose_life))
 
-            tp_max = self.turning_point_max.loc[date]
-            tp_min = self.turning_point_max.loc[date]
-            tech_indicator = self.tech_indicator_matrix.loc[date]
-            s = np.concatenate((tp_max.values.flatten(), tp_min.values.flatten(), tech_indicator.values.flatten(),
-                                profit_bin.values.flatten()), axis=0)
-            state = self.State(date, s)
-            print("generated sellSignalStates, date " + str(date))
-            return state
-        except KeyError:
-            print("ERROR getting sell signal state for date " + str(date))
-            return None
+    # Sample and fit
+    batch = ring_buf.sample_batch(32)
+    loss = fit_batch(model, target_model, batch[0], one_hot_encoder.transform(batch[1].reshape(-1,1)), 
+              batch[2], batch[3], batch[4], pre_process)
+    return loss
 
-    def get_buy_signal_states_by_date(self, date):
-        try:
-            tp_max = self.turning_point_max.loc[date]
-            tp_min = self.turning_point_max.loc[date]
-            tech_indicator = self.tech_indicator_matrix.loc[date]
-            s = np.concatenate((tp_max.values.flatten(), tp_min.values.flatten(), tech_indicator.values.flatten()),
-                               axis=0)
-            state = self.State(date, s)
-            print("generated buySignalStates, date " + str(date))
-            return state
-        except KeyError:
-            print("ERROR getting buy signal state for date " + str(date))
-            return None
 
-    def get_sell_order_states_by_date(self, date):
-        # get next day state, if next day state is not available, throws error
+def evaluate(logger, model, agent, pre_process, env):
 
-        try:
-            tech_indicator = self.tech_indicator_matrix.loc[date]
-            s = tech_indicator.values.flatten()
-            state = self.State(date, s)
-            print("generated sellOrderStates, date " + str(date))
-            return state
-        except KeyError:
-            print("ERROR getting sell order state for date " + str(date))
-            return None
+    print("Evaluation started.")
+    for i in range(0, logger.evaluation_number):
+        agent.reset_env(env, pre_process)
 
-    def get_buy_order_states_by_date(self, date):
-        # get next day state, if next day state is not available, throws error
-
-        try:
-            tech_indicator = self.tech_indicator_matrix.loc[date]
-            s = tech_indicator.values.flatten()
-            state = self.State(date, s)
-            print("generated buyOrderStates, date " + str(date))
-            return state
-        except KeyError:
-            print("ERROR getting buy order state for date " + str(date))
-            return None
-
-    def load_data(self):
-        # load from csv/ db
-        pass
-
-    def get_market_data_by_date(self, date):
-        market_data = self.data.loc[date]
-        print("Getting market data, date: " + str(date) + " , " + str(market_data))
-
-        return market_data
-
-    def produce_state(self, agent, last_date):
-        if isinstance(agent, BuyOrderAgent):
-            s = self.get_buy_order_states_by_date(last_date + 1)
-        elif isinstance(agent, SellOrderAgent):
-            s = self.get_sell_order_states_by_date(last_date + 1)
-        elif isinstance(agent, BuySignalAgent):
-            if last_date is None:
-                # randomly pick a day from dataset
-                # date =98
-                date = self.data.sample().index.values[0]
-                print("generated buySignalStates - first")
-                s = self.get_buy_signal_states_by_date(date)
-            else:
-                s = self.get_buy_signal_states_by_date(last_date + 1)
-        elif isinstance(agent, SellSignalAgent):
-            s = self.get_sell_signal_states_by_date(agent.bp, last_date + 1)
-
-        if s is not None:
-            print(str(s.date) + " , " + str(s.value))
-        return s
+        while not agent.is_terminate:
+            action = choose_best_action(model.model, agent.end_states, env.action_space.n)
+            agent.do_action(env, pre_process, action)
+            logger.write(agent)
